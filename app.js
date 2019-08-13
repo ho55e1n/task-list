fa63dbf71966aa17807a831d744ff8618dbb9512
Hoss <mh.sharifkazemi@gmail.com>
2019-08-13 23:53:04 +1000

    added removeTask, clearTasks, filterTasks

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners() {
   //Add task Event
   form.addEventListener("submit", addTask);
+  //Remove Task Event
+  taskList.addEventListener("click", removeTask);
+  // Clear Task Event
+  clearBtn.addEventListener("click", clearTasks);
+  // Filter tasks event
+  filter.addEventListener("keyup", filterTasks);
 }
 
 //Add Task
@@ -39,4 +45,40 @@ function addTask(e) {
   taskInput.value = "";
 
   e.preventDefault();
+}
+
+//Remove Task
+function removeTask(e) {
+  console.log(e.target);
+
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Clear Tasks
+function clearTasks() {
+  //Slower solution
+  //taskList.innerHTML = "";
+
+  // Faster method
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  console.log(text);
+  document.querySelectorAll(".collection-item").forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
